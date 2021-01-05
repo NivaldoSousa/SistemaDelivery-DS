831beb51bdec51de736dffcb204f5db36f573ffe
NGA Sousa <nillsousa026@gmail.com>
2021-01-04 23:06:39 -0300

    Initial setup

---
+++ backendsdeliver/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dsdeliver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+//Classe de Liberaçao dos Cors, que por motivos de segurança o browser bloqueia
+// quando uma aplicaçao web esta acessando uma outra de outro dominio ou seja quando o front end esta hospedado em
+// um lugar e o back-end em outro.
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    // essa configuraçao e para o gerenciamento do banco de dados de teste do H2,
+    // sem ela nao ha um bom funcionamento    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.cors().and().csrf().disable(); // desabilitando o csrf = ataque baseado em sessao
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll(); // permitindo todo tipo de requisiçao
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
