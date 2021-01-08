b3d25e8f9637b992db11a07841f89c3e0600e798
NGA Sousa <nillsousa026@gmail.com>
2021-01-08 20:44:31 -0300

    GET /oders

--- backendsdeliver/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backendsdeliver/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -1,20 +1,19 @@
-package com.devsuperior.dsdeliver.entities;
+package com.devsuperior.dsdeliver.dto;
 
-import javax.persistence.*;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Entity
-@Table(name = "tb_order")
-public class Order implements Serializable {
+public class OrderDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String address;
     private Double latitude;
@@ -22,24 +21,29 @@ public class Order implements Serializable {
     private Instant moment;
     private OrderStatus status;
 
-    //essa funçao faz com que nao haja repetição do mesmo produto no mesmo pedido
-    // e a funçao SET tem essa finalidade, nao aceita repetiçao,
-    // e tambem ele ja cria no banco a relaçao entre as entidade, de um para muitos (OneToMany).
-   @ManyToMany
-   @JoinTable(name = "tb_order_product",
-   joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
-    private Set<Product> products = new HashSet<>();
+    private List<ProductDTO> products = new ArrayList<>();
 
-    public Order(){
+    public OrderDTO(){
     }
 
-    public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
         this.id = id;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
         this.moment = moment;
         this.status = status;
+    }
+
+    public OrderDTO(Order entity) {
+        id = entity.getId();
+        address = entity.getAddress();
+        latitude = entity.getLatitude();
+        longitude = entity.getLongitude();
+        moment = entity.getMoment();
+        status = entity.getStatus();
+        products = entity.getProducts().stream()
+                .map(x -> new ProductDTO(x)).collect(Collectors.toList());
     }
 
     public Long getId() {
@@ -90,24 +94,9 @@ public class Order implements Serializable {
         this.status = status;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Order order = (Order) o;
-        return Objects.equals(id, order.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public Set<Product> getProducts() {
+    public List<ProductDTO> getProducts() {
         return products;
     }
 
-    public void setProducts(Set<Product> products) {
-        this.products = products;
-    }
+
 }
---
+++ backendsdeliver/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsdeliver.services;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Transactional
+    public List<OrderDTO> findAll(){
+        List<Order> list = repository.findOrdersWithProducts();
+        return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); // recebendo a lista de produtos e convertendo(stream)
+        //os dados obtidos e mapeando(map) para um productDTO e voltando a ser uma list novamente so que sendo uma list productDTO
+    }
+
+
+}
