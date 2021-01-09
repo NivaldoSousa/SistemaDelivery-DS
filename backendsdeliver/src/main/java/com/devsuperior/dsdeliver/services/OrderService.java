bbf92ea0ee1f18271d75e6e6306aed18011e1f5e
NGA Sousa <nillsousa026@gmail.com>
2021-01-08 21:12:18 -0300

    POST /orders

--- backendsdeliver/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backendsdeliver/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsdeliver.services;
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
 import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
 import com.devsuperior.dsdeliver.repositories.ProductRepository;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,12 +21,28 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
-    @Transactional
-    public List<OrderDTO> findAll(){
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public List<OrderDTO> findAll() {
         List<Order> list = repository.findOrdersWithProducts();
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); // recebendo a lista de produtos e convertendo(stream)
         //os dados obtidos e mapeando(map) para um productDTO e voltando a ser uma list novamente so que sendo uma list productDTO
     }
 
+    // salvar pedido junto com os produtos
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+        //associando os produtos para o pedido quando for salvar
+        for (ProductDTO p : dto.getProducts()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        repository.save(order);
+        return new OrderDTO(order);
+    }
 
 }
