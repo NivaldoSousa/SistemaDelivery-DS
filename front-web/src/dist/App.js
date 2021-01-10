f5cbeb6d617a35b75a6421210507e306ac961b38
NGA Sousa <nillsousa026@gmail.com>
2021-01-10 11:03:10 -0300

    Add Footer

---
+++ front-web/src/dist/App.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+require("./App.css");
+var Home_1 = require("./Home");
+var Navbar_1 = require("./Navbar");
+function App() {
+    return (React.createElement(React.Fragment, null,
+        React.createElement(Navbar_1["default"], null),
+        React.createElement(Home_1["default"], null)));
+}
+exports["default"] = App;
