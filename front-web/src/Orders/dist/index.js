7a559ee71a96a02edab5a6529d1bfe69a9597f9b
nills <nillsousa026@gmail.com>
2021-03-03 11:50:30 -0300

    Acertos

--- front-web/src/Orders/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("./styles.css");
-function Orders() {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "Orders")));
-}
-exports["default"] = Orders;
